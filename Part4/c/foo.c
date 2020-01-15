f836e4e4ff7c7c079b2288cd0fbcd9e8a56af677
Michael <54835557+MainMike@users.noreply.github.com>
2020-01-15 23:05:25 +0100

    Update foo.c

--- Part4/c/foo.c
+++ Part4/c/foo.c
@@ -6,23 +6,32 @@ int i = 0;
 // Note the return type: void*
 void* incrementingThreadFunction(){
     // TODO: increment i 1_000_000 times
+    long k;
+    for(k=0;k<1000000;k++)
+        i++;
     return NULL;
 }
 
 void* decrementingThreadFunction(){
     // TODO: decrement i 1_000_000 times
+    long k;
+    for(k=0;k<1000000;k++)
+        i--;
     return NULL;
 }
 
 
 int main(){
     // TODO: declare incrementingThread and decrementingThread (hint: google pthread_create)
+    pthread_t incrementingThread,decrementingThread;
+
+
     pthread_create(&incrementingThread, NULL, incrementingThreadFunction, NULL);
     pthread_create(&decrementingThread, NULL, decrementingThreadFunction, NULL);
-    
+
     pthread_join(incrementingThread, NULL);
     pthread_join(decrementingThread, NULL);
-    
+
     printf("The magic number is: %d\n", i);
     return 0;
 }
